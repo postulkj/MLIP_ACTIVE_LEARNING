3bb76a23d05967cdcbc14aafb1f50133b3254591
postulkj <h.postulka@seznam.cz>
2025-09-17 11:52:35 +0200

    formated

--- stde_on_set.py
+++ stde_on_set.py
@@ -1,29 +1,38 @@
 #!/home/postulka/anaconda3/envs/gmnn_gpu/bin/python
 
 #this script load .npz file with geometric data and computes stde for energy and forces for ecah structure
-
-from gmnn.calculators import ASECalculator
 import numpy as np
 
-#loadd data
-data = np.load('data.npz', allow_pickle=True)
+from gmnn.calculators import ASECalculator
 
-R, Z, N, C, E = arrays["R"], arrays["Z"], arrays["N"], arrays["C"], arrays["E"]
-#first atoms object from the set
+def compute_stde(data_file, output_file='stde_results.txt', model_path, config_file):
+    HARTREE_TO_KCALMOL = 627.51
+#loadd data
+    data = np.load(data_file, allow_pickle=True)
+
+    R, Z, N, C, E = arrays["R"], arrays["Z"], arrays["N"], arrays["C"], arrays["E"]
 
 #loop through the structures
-for i in range(len(N)):
-    numb = [ i for i  in data["Z"][index] if i != 0.0 ]
-    pos = data["R"][index][0:len(numb)]  #[ j for i,j  in zip(data["Z"][index],data["R"][index]) if i != 0.0 ]
-    atoms = Atoms(numbers=numb, positions=pos)
-    if i == 0:
-        calc = ASECalculator(atoms=atoms, model_path="/home/postulka/HF_BROKEN_SYMMETRY/TRAIN/acetaldehyde/models/just_energy", config="pes_just_energy.txt", device_number=0, use_all_features=True, wrap_positions=False, compute_stress=False, set_units="kcal/mol to kcal/mol")
-        print("Structure index, stde energy, stde forces")
-        continue
-    atoms.set_calculator(calc)
-    variance_f = calc.get_force_variance(atoms)
-    stde_f = np.sqrt(variance).mean()
-    variance_e = calc.get_energy_variance(atoms)
-    stde_e = np.sqrt(variance)
-    print(f"{i} {stde_e} {stde_f}")
+    with open(output_file, 'w') as f_out:
+        for i in range(len(N)):
+            numb = [ i for i  in data["Z"][i] if i != 0.0 ]
+            pos = data["R"][i][0:len(numb)]  #[ j for i,j  in zip(data["Z"][index],data["R"][index]) if i != 0.0 ]
+            atoms = Atoms(numbers=numb, positions=pos)
+            if i == 0:
+                natom=len(numb)
+                calc = ASECalculator(atoms=atoms, model_path=model_path, config=config_file, device_number=0, use_all_features=True, wrap_positions=False, compute_stress=False, set_units="kcal/mol to Hartree")
+                f_out.write("#Structure index, natom, stde energy, stde forces")
+                continue
+            if natom!=len(numb):
+                natom = len(numb)
+                calc = ASECalculator(atoms=atoms, model_path=model_path, config=config_file, device_number=0, use_all_features=True, wrap_positions=False, compute_stress=False, set_units="kcal/mol to Hartree")
+            atoms.set_calculator(calc)
+            variance_f = calc.get_force_variance(atoms)*HARTREE_TO_KCALMOL
+            stde_f = np.sqrt(variance_f).mean()
+            variance_e = calc.get_energy_variance(atoms)*HARTREE_TO_KCALMOL
+            stde_e = np.sqrt(variance_e)
+            f_out.write(f"{i} {natom} {stde_e} {stde_f}\n") 
 
+if __name__ == "__main__":
+    data_file = 'data.npz'
+    compute_stde(data_file, output_file='stde_results.txt',f'/home/postulka/HF_BROKEN_SYMMETRY/TRAIN/acetaldehyde/models/move_size_7500',f'pes_move_size_7500.txt')
